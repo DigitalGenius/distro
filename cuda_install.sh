dc37476bcccdc2af07d8ed8bacd9d5e86f0e9a7c
jmrf <jmrf@users.noreply.github.com>
2016-06-17 12:23:15 +0100

    Update and rename travis_cuda_install.sh to cuda_install.sh

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 if [[ `uname` == 'Linux' ]]; then
-  CUDA_VERSION=6-5
-  CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_6.5-14_amd64.deb
+  CUDA_VERSION=7-5
+  CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
   CUDA_FILE=/tmp/cuda_install.deb
 
   curl $CUDA_URL -o $CUDA_FILE
